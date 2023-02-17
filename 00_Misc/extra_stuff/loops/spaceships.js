42c1501d14992536dbaf043fca7786c7799d8be2
Andreas Simmelkiaer <andreas.sim@live.dk>
2023-02-17 13:38:53 +0100

    More exercises

--- 00_Misc/extra_stuff/loops/spaceships.js
+++ 00_Misc/extra_stuff/loops/spaceships.js
@@ -1,7 +1,7 @@
 const spaceships = [
-    { name: "Chinese Weather Balloon", isPriceless: false},
-    { name: undefined, type: "UFO", isPriceless: true},
-    { name: "Apollo 13", versionNumber: 13, isPriceless: false}
+    { name: "Chinese Weather Balloon", isPriceless: false, cost: 1_000_000},
+    { name: undefined, type: "UFO", isPriceless: true, cost: Number.MAX_SAFE_INTEGER},
+    { name: "Apollo 13", versionNumber: 13, isPriceless: false, cost: 2_000_000}
 ];
 
 //spaceships.forEach((spaceship, index) => console.log(index));
@@ -17,3 +17,7 @@ const pricelessSpaceships = spaceships.filter(spaceship => spaceship.isPriceless
 const pricedSpaceships = spaceships.filter(spaceship => spaceship.isPriceless === false);
 console.log(pricedSpaceships);
 console.log(pricelessSpaceships);
+
+
+const affordableSpaceship = spaceships.find(spaceship => spaceship.cost <= 2_000_000);
+console.log(affordableSpaceship);
